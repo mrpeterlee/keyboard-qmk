5adb5bce5678ad19933a7677bd201a896da5b8f5
Peter Lee <peter.lee@astrocapital.net>
2025-02-06 00:40:09 -0500

    added perc in num

--- keyboards/zsa/voyager/keymaps/getreuer/keymap.c
+++ keyboards/zsa/voyager/keymaps/getreuer/keymap.c
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                       KC_EQL , KC_SLSH, KC_ASTR, KC_MINS, XXXXXXX, KC_MINS,
                       KC_EQL , KC_7   , KC_8   , KC_9   , KC_LPRN, KC_RPRN,
-                      KC_DOT , KC_4   , KC_5   , KC_6   , KC_PLUS, XXXXXXX,
+                      KC_DOT , KC_4   , KC_5   , KC_6   , KC_PLUS, KC_PERC,
                       XXXXXXX, KC_1   , KC_2   , KC_3   , KC_SLSH, XXXXXXX,
              _______, KC_0   
   ),
