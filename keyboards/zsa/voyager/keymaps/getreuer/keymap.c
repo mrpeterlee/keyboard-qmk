e58434d2ae7386dd9ad5c9605c7c7e4c23eed654
Peter Lee <peter.lee@astrocapital.net>
2025-02-11 07:11:14 +0000

    improved symbols

--- keyboards/zsa/voyager/keymaps/getreuer/keymap.c
+++ keyboards/zsa/voyager/keymaps/getreuer/keymap.c
@@ -71,16 +71,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [SYM1] = LAYOUT_LR(  // Symbol layer.
-    XXXXXXX, KC_F1  , KC_F2  , KC_F3     , KC_F4  , KC_F5  ,
-    XXXXXXX, XXXXXXX, KC_AT  , KC_HASH   , KC_ASTR, G(S(KC_S)),
-    TMUXESC, KC_AMPR, KC_EXLM, KC_KP_PLUS, KC_MINS, XXXXXXX,
+    XXXXXXX, KC_F1  , KC_F2  , KC_F3     , KC_F4     , KC_F5  ,
+    XXXXXXX, XXXXXXX, KC_AT  , KC_HASH   , XXXXXXX   , G(S(KC_S)),
+    TMUXESC, KC_AMPR, KC_EXLM, KC_QUES   , KC_MINS   , XXXXXXX,
     XXXXXXX, XXXXXXX, KC_TILD, KC_PIPE   , C(S(KC_V)), XXXXXXX,
                                                  _______, _______,
 
                       KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-                      KC_EQL , KC_LBRC, KC_RBRC, KC_UNDS, XXXXXXX, XXXXXXX,
-                      KC_CIRC, KC_LPRN, KC_RPRN, KC_DLR , KC_QUES, QK_LLCK,
-                      KC_PERC, KC_LCBR, KC_RCBR, KC_BSLS, XXXXXXX, XXXXXXX,
+                      KC_EQL , KC_LBRC, KC_RBRC, KC_UNDS, KC_PERC, XXXXXXX,
+                      KC_CIRC, KC_LPRN, KC_RPRN, KC_DLR , KC_KP_PLUS, QK_LLCK,
+                      KC_ASTR, KC_LCBR, KC_RCBR, KC_BSLS, KC_SLSH, XXXXXXX,
              C(KC_BSPC), C(KC_F)
   ),
 
@@ -105,10 +105,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                                  _______, _______,
 
-                      XXXXXXX, KC_7   , KC_8   , KC_9   , XXXXXXX, KC_MINS,
-                      KC_EQL , KC_4   , KC_5   , KC_6   , KC_LPRN, KC_RPRN,
-                      KC_PLUS, KC_1   , KC_2   , KC_3   , KC_ASTR, QK_LLCK,
-                      KC_PERC, KC_0   , KC_MINS, KC_DOT , KC_SLSH, XXXXXXX,
+                      XXXXXXX, KC_7   , KC_8   , KC_9   , KC_MINS, KC_MINS,
+                      KC_EQL , KC_4   , KC_5   , KC_6   , KC_PERC, XXXXXXX,
+                      KC_MINS, KC_1   , KC_2   , KC_3   , KC_KP_PLUS, QK_LLCK,
+                      KC_ASTR, KC_0   , KC_MINS, KC_DOT , KC_SLSH, XXXXXXX,
              _______, _______
   ),
 
