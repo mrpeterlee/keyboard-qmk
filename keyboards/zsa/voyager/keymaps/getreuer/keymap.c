e65ef60bfc3f4d7ac8144c95eea1bcddd5196f99
Peter Lee <peter.lee@astrocapital.net>
2025-06-04 00:48:59 +0000

    remap comma key

--- keyboards/zsa/voyager/keymaps/getreuer/keymap.c
+++ keyboards/zsa/voyager/keymaps/getreuer/keymap.c
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                       XXXXXXX   , KC_SLSH, KC_ASTR, KC_LPRN, KC_RBRC   , KC_MINS,
                       KC_EQL    , KC_7   , KC_8   , KC_9   , KC_PERC   , XXXXXXX,
-                      KC_KP_PLUS, KC_4   , KC_5   , KC_6   , KC_MINS   , KC_DOT ,
+                      KC_KP_PLUS, KC_4   , KC_5   , KC_6   , KC_MINS   , KC_COMM,
                       KC_ASTR   , KC_1   , KC_2   , KC_3   , KC_SLSH   , QK_LLCK,
              _______, KC_0   
   ),
